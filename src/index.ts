940f848a05f4917070dd02feb891983d5991c424
Dzevad <dzevad.del@gmail.com>
2022-03-21 01:40:20 +0100

    finished mobile and desktop design

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import emailValidator from 'email-validator';
+
+const emailFieldBox = document.querySelector(
+  '.email-form__input-box'
+)! as HTMLDivElement;
+
+const emailField = document.querySelector(
+  '.email-form__field'
+)! as HTMLInputElement;
+
+const emailErrorEl = document.querySelector(
+  '.email-form__error'
+)! as HTMLSpanElement;
+
+const form = document.querySelector('.email-form')! as HTMLFormElement;
+
+let email = '';
+
+emailField.addEventListener(
+  'change',
+  (e) => (email = (e.target as HTMLInputElement).value)
+);
+
+form.addEventListener('submit', (e): void => {
+  e.preventDefault();
+
+  // validate email input
+  if (!emailValidator.validate(email)) {
+    emailErrorEl.innerHTML = 'Please provide valid email address.';
+  } else {
+    emailErrorEl.innerHTML = '';
+  }
+});
